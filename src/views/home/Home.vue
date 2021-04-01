b065db5661541da9cd596eed862b94209369af65
sola-grj <1131631886@qq.com>
2021-04-01 18:04:01 +0800

    'navbar初始化'

---
+++ src/views/home/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+  import {getHomeMultidata} from "network/home";
+
+  export default {
+    name: "Home",
+    data(){
+      return {
+        banners:[],
+        recommends:[]
+      }
+    },
+    components:{
+      NavBar
+    },
+    created() {
+      // 组件创建完成后发送网络请求
+      // 1.请求多个数据
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
+    }
+  }
+</script>
+
+<style scoped>
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
